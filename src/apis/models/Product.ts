5b7baa17b57fc7477619cc9f25d6ca9ecda58d71
Bouaziz Mohamed Cherif <74194285+Med-Cherif@users.noreply.github.com>
2022-02-06 23:09:49 +0100

    Update Product.ts

--- src/apis/models/Product.ts
+++ src/apis/models/Product.ts
@@ -2,21 +2,46 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
     productCode: { type: String, required: true },
-    description: { type: mongoose.Schema.Types.Mixed, default: {} },
+    description: { type: String },
+    type: { type: String }, // like (phone, computer, clothes)
     name: { type: String, required: true },
+    specificOptions: {
+        type: Map,
+        of: mongoose.Schema.Types.Mixed
+    },
     price: { type: Number, required: true, default: 0 },
     qty: { type: Number, default: 1 },
     image: { type: String },
-    categories: { type: Array, default: [], enum: [
-        'phones',
-        'phones & computers accessoires',
-        'computers',
-        'women clothes',
-        'man clothes',
-        'fashion',
-        'cosmetics & beauty'
-    ]}
+    categories: { type: Array, default: [] }
 })
+
+/*  example: phone
+    specificOptions: {
+        brand: apple,
+        marque: iphone 12,
+        ficheT: {
+            camera: bla bla,
+            ram: xx GB,
+        }
+    }
+    
+    example: t-shirt
+    specificOptions: {
+        brand: adidas,
+        size: 'm',
+    }
+*/
+
+/*
+    'phones',
+    'phones & computers accessoires',
+    'computers',
+    'women clothes',
+    'man clothes',
+    'fashion',
+    'cosmetics & beauty'
+
+*/
 
 const ProductModel = mongoose.model('Product', productSchema)
 
